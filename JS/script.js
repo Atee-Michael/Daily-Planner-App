6bf7953755bfe0d17d25ed95993da26751758fca
Michael Atee <ateemichael@yahoo.com>
2023-01-24 00:28:08 +0000

    Added pseudocode/logic to JS

---
+++ JS/script.js
@@ -0,0 +1,30 @@
+// Get current day and display it at the top of the calendar
+
+// Create timeblocks for standard business hours
+
+// Create the time label
+
+  // Create the event input field
+
+  // Create the save button
+
+  // Add the timeblock to the container
+
+  // Check the current time and color-code the timeblock accordingly
+
+  // Check for saved events in local storage
+
+// Save event function
+
+  // Get the time of the event
+
+  // Get the value of the event input field
+
+  // Save the event in local storage
+
+
+
+
+
+
+
